970027eb3ab9a93bfedcf95ba66ce996e0459747
Ben Newhouse <ben@bubb.li>
2014-03-28 17:58:57 -0700

    Refactored things out into a channel select statement to avoid concurrency issues with termbox

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"github.com/nsf/termbox-go"
 	"os"
@@ -14,6 +15,7 @@ type ProgramState struct {
 	buffer               []string
 	timestamps           []int64
 	log                  map[string]*HistoricalVariable
+	log_keys             []string // Kept around to keep order
 	focused_pane         int
 	selected_index       int
 	selected_buffer_line int
@@ -48,11 +50,14 @@ func draw_all(state *ProgramState) {
 	width, height := termbox.Size()
 
 	// Draw the buffer
-	for i := state.offset; i < state.offset+height-1; i++ {
+	for i := state.offset; i < min(state.offset+height-1, len(state.buffer)); i++ {
 		if i > state.selected_buffer_line {
 			break
 		}
 		for j, c := range state.buffer[i] {
+			if j == width/2 {
+				break
+			}
 			termbox.SetCell(j, i-state.offset, c, termbox.ColorDefault, termbox.ColorDefault)
 		}
 	}
@@ -64,7 +69,9 @@ func draw_all(state *ProgramState) {
 
 	// Draw the variables
 	i := 0
-	for _, v := range state.log {
+	for _, k := range state.log_keys {
+		v := state.log[k]
+
 		if !state.realtime && state.timecursor < v.Timestamp[0] {
 			continue
 		}
@@ -150,6 +157,10 @@ func scroll_to_time(timestamp int64, current_offset int, timestamps *[]int64) in
 }
 
 func main() {
+	var prefix = flag.String("prefix", ".", "Prefix used to match lines")
+	var delimeter = flag.String("delimeter", "=", "Prefix used to match lines")
+	flag.Parse()
+
 	err := termbox.Init()
 	if err != nil {
 		panic(err)
@@ -162,6 +173,7 @@ func main() {
 		[]string{},
 		[]int64{},
 		map[string]*HistoricalVariable{},
+		[]string{},
 		1,
 		0,
 		0,
@@ -170,6 +182,9 @@ func main() {
 		time.Now().UnixNano(),
 	}
 
+	stdin_channel := make(chan string)
+	input_channel := make(chan termbox.Event)
+
 	go func() {
 		bio := bufio.NewReader(os.Stdin)
 		for {
@@ -177,9 +192,23 @@ func main() {
 			if err != nil {
 				break
 			}
+			stdin_channel <- string(line)
+		}
+	}()
+
+	go func() {
+		for {
+			input_channel <- termbox.PollEvent()
+		}
+	}()
+
+loop:
+	for {
+		select {
+		case line := <-stdin_channel:
 			timestamp := time.Now().UnixNano()
-			if len(line) > 0 && line[0] == '.' {
-				slices := strings.SplitN(strings.TrimPrefix(string(line), "."), " ", 2)
+			if len(line) > 0 && strings.HasPrefix(line, *prefix) {
+				slices := strings.SplitN(strings.TrimPrefix(string(line), *prefix), *delimeter, 2)
 				if len(slices) == 2 {
 					existing := state.log[slices[0]]
 					if existing == nil {
@@ -188,6 +217,7 @@ func main() {
 							[]int64{},
 							0}
 						state.log[slices[0]] = existing
+						state.log_keys = append(state.log_keys, slices[0])
 					}
 					existing.Values = append(existing.Values, slices[1])
 					existing.Timestamp = append(existing.Timestamp, timestamp)
@@ -205,97 +235,83 @@ func main() {
 				state.selected_buffer_line = len(state.buffer) - 1
 			}
 			draw_all(&state)
-		}
-	}()
 
-loop:
-	for {
-		switch ev := termbox.PollEvent(); ev.Type {
-		case termbox.EventKey:
-			switch ev.Key {
-			case termbox.KeyEsc:
-				break loop
-			case termbox.KeyArrowDown:
-				switch state.focused_pane {
-				case 0:
-					state.realtime = false
-					state.selected_buffer_line = min(state.selected_buffer_line+1, len(state.buffer)-1)
+		case ev := <-input_channel:
+			switch ev.Type {
+			case termbox.EventKey:
+				switch ev.Key {
+				case termbox.KeyEsc:
+					break loop
+				case termbox.KeyArrowDown:
+					switch state.focused_pane {
+					case 0:
+						state.realtime = false
+						state.selected_buffer_line = min(state.selected_buffer_line+1, len(state.buffer)-1)
 
-					_, height := termbox.Size()
-					state.timecursor = state.timestamps[state.selected_buffer_line]
-					state.offset = max(state.selected_buffer_line-height+2, 0)
-					seek_to_time(state.timecursor, &state.log)
-				case 1:
-					state.selected_index = min(state.selected_index+1, len(state.log)-1)
-				}
-			case termbox.KeyArrowUp:
-				switch state.focused_pane {
-				case 0:
-					state.realtime = false
-					state.selected_buffer_line = max(0, state.selected_buffer_line-1)
-
-					_, height := termbox.Size()
-					state.timecursor = state.timestamps[state.selected_buffer_line]
-					state.offset = max(state.selected_buffer_line-height+2, 0)
-					seek_to_time(state.timecursor, &state.log)
-				case 1:
-					state.selected_index = max(0, state.selected_index-1)
-				}
-			case termbox.KeyArrowLeft:
-				if state.focused_pane == 1 {
-					state.realtime = false
-					i := 0
-
-					// Figure out where the new timecursor should be
-					for _, v := range state.log {
-						if i == state.selected_index {
-							v.Focused = max(0, min(len(v.Values)-1, v.Focused-1))
-							state.timecursor = v.Timestamp[v.Focused]
-							break
-						}
-						i += 1
+						_, height := termbox.Size()
+						state.timecursor = state.timestamps[state.selected_buffer_line]
+						state.offset = max(state.selected_buffer_line-height+2, 0)
+						seek_to_time(state.timecursor, &state.log)
+					case 1:
+						state.selected_index = min(state.selected_index+1, len(state.log)-1)
 					}
+				case termbox.KeyArrowUp:
+					switch state.focused_pane {
+					case 0:
+						state.realtime = false
+						state.selected_buffer_line = max(0, state.selected_buffer_line-1)
 
-					// Update
-					_, height := termbox.Size()
-					state.selected_buffer_line = scroll_to_time(state.timecursor, state.selected_buffer_line, &state.timestamps)
-					state.offset = max(state.selected_buffer_line-height+1, 0)
-					seek_to_time(state.timecursor, &state.log)
-				}
-			case termbox.KeyArrowRight:
-				if state.focused_pane == 1 {
-					state.realtime = false
-					i := 0
-
-					// Figure  out where the new timecursor should be
-					for _, v := range state.log {
-						if i == state.selected_index {
-							v.Focused = max(0, min(len(v.Values)-1, v.Focused+1))
-							state.timecursor = v.Timestamp[v.Focused]
-							break
-						}
-						i += 1
+						_, height := termbox.Size()
+						state.timecursor = state.timestamps[state.selected_buffer_line]
+						state.offset = max(state.selected_buffer_line-height+2, 0)
+						seek_to_time(state.timecursor, &state.log)
+					case 1:
+						state.selected_index = max(0, state.selected_index-1)
 					}
+				case termbox.KeyArrowLeft:
+					if state.focused_pane == 1 {
+						state.realtime = false
 
-					// Update
-					_, height := termbox.Size()
-					state.selected_buffer_line = scroll_to_time(state.timecursor, state.selected_buffer_line, &state.timestamps)
-					state.offset = max(state.selected_buffer_line-height+1, 0)
-					seek_to_time(state.timecursor, &state.log)
+						// Figure out where the new timecursor should be
+						v := state.log[state.log_keys[state.selected_index]]
+						v.Focused = max(0, min(len(v.Values)-1, v.Focused-1))
+						state.timecursor = v.Timestamp[v.Focused]
+
+						// Update
+						_, height := termbox.Size()
+						state.selected_buffer_line = scroll_to_time(state.timecursor, state.selected_buffer_line, &state.timestamps)
+						state.offset = max(state.selected_buffer_line-height+1, 0)
+						seek_to_time(state.timecursor, &state.log)
+					}
+				case termbox.KeyArrowRight:
+					if state.focused_pane == 1 {
+						state.realtime = false
+
+						// Figure  out where the new timecursor should be
+						v := state.log[state.log_keys[state.selected_index]]
+						v.Focused = max(0, min(len(v.Values)-1, v.Focused+1))
+						state.timecursor = v.Timestamp[v.Focused]
+
+						// Update
+						_, height := termbox.Size()
+						state.selected_buffer_line = scroll_to_time(state.timecursor, state.selected_buffer_line, &state.timestamps)
+						state.offset = max(state.selected_buffer_line-height+1, 0)
+						seek_to_time(state.timecursor, &state.log)
+					}
+				case termbox.KeySpace:
+					state.focused_pane = (state.focused_pane + 1) % 2
+				case termbox.KeyCtrlR:
+					state.realtime = true
+					for _, v := range state.log {
+						v.Focused = len(v.Values) - 1
+					}
+					state.selected_buffer_line = len(state.buffer) - 1
 				}
-			case termbox.KeySpace:
-				state.focused_pane = (state.focused_pane + 1) % 2
-			case termbox.KeyCtrlR:
-				state.realtime = true
-				for _, v := range state.log {
-					v.Focused = len(v.Values) - 1
-				}
-				state.selected_buffer_line = len(state.buffer) - 1
+				draw_all(&state)
+			case termbox.EventMouse:
+			case termbox.EventResize:
+				draw_all(&state)
 			}
-			draw_all(&state)
-		case termbox.EventMouse:
-		case termbox.EventResize:
-			draw_all(&state)
 		}
 	}
 }
